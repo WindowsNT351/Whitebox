e6d8f36c159b2b9da08a4a27ef9ec303a4084144
WindowsNT351 <96978173+WindowsNT351@users.noreply.github.com>
2025-08-08 20:00:31 +0800

    100B102

--- codes/main.cpp
+++ codes/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <Windows.h>
 #include <iostream>
+#include <afxwin.h>
 using namespace std;
 
 void astrcpy(char* out, char* in, int addr)
@@ -11,6 +11,61 @@ void astrcpy(char* out, char* in, int addr)
 	{
 		out[j] = in[i];
 		j++;
+	}
+}
+
+bool DeleteDirectory(CString DirName)
+{
+	CString PUBPATH;
+	PUBPATH = DirName;
+	CFileFind tempFind;
+	DirName += "\\*.*";
+	BOOL IsFinded = (BOOL)tempFind.FindFile(DirName);
+	//cout << IsFinded <<endl;
+	while (IsFinded)
+	{
+		IsFinded = (BOOL)tempFind.FindNextFile();
+		if (!tempFind.IsDots())
+		{
+			CString strDirName;
+			strDirName += PUBPATH;
+			strDirName += "\\";
+			strDirName += tempFind.GetFileName();
+			if (tempFind.IsDirectory())
+			{
+				DeleteDirectory(strDirName);
+			}
+			else
+			{
+				SetFileAttributes(strDirName, FILE_ATTRIBUTE_NORMAL); //去掉文件的系统和隐藏属性
+				DeleteFile(strDirName);
+			}
+		}
+	}
+	tempFind.Close();
+	if (!RemoveDirectory(PUBPATH))
+	{
+		return false;
+	}
+	return true;
+
+}
+
+void printPrct(int log, int prct)
+{
+	prct++;
+	for (int i = 0; i < log; i++)
+	{
+		printf("\b");
+	}
+	int progLong = prct * log / 100;
+	for (int i = 0; i < progLong; i++)
+	{
+		printf("*");
+	}
+	for (int i = 0; i < log - progLong; i++)
+	{
+		printf("-");
 	}
 }
 
@@ -95,30 +150,21 @@ int main(int argc,char **argv)
 			"      when <destination> specifies a file.\n"
 			"========================================================================================\n"
 		);
-		puts("Version:100B101        With CE-Collections:N/A");
+		puts("Version:100B102        With CE-Collections:202B104");
 		puts("Copyright 351Workshop 2025");
 		return 1;
 	}
 
-	char nkPath[255] = { 0 };
-	strcpy_s(nkPath, argv[argc-1]);
-	char imgPath[255] = { 0 };
-	char emulPath[255] = { 0 };
-	char emulDir[255] = "./";
-	GetPrivateProfileString("Img", "imgPath", "./samp.img", imgPath, 255, "./whitebox.ini");
-	GetPrivateProfileString("86Box", "emulPath", "86Box.exe", emulPath, 255, "./whitebox.ini");
-	GetPrivateProfileString("86Box", "emulDir", "./", emulDir, 255, "./whitebox.ini");
-	long long int nkAddrOffset = GetPrivateProfileInt("Img", "nkAddrOffset", 0x81000, "./whitebox.ini");
-	long long int batAddrOffset = GetPrivateProfileInt("Img", "batAddrOffset", 0x4081000, "./whitebox.ini");
-
 
 	char batBuffer[120] = { 0 };
-	char profilePath[255] = "PRO1.cfg";
+	char profilePath[255] = ".\\PRO1.cfg";
 	strcat_s(batBuffer, "loadcepc ");
 	for (int i = 0; i < argc - 2; i++)
 	{
 		if (argv[i + 1][1] == 'M' || argv[i + 1][1] == 'm')
 		{
+			memset(profilePath, 0, 120);
+			strcat_s(profilePath, ".\\");
 			astrcpy(profilePath, argv[i + 1], 3);
 		}
 		else
@@ -126,22 +172,48 @@ int main(int argc,char **argv)
 			strcat_s(batBuffer, argv[i + 1]);
 			strcat_s(batBuffer, " ");
 		}
-	
+
 	}
 	strcat_s(batBuffer, "C:\\ce\\nk.bin");
 
-	printf("=======================================================\n");
+
+	char nkPath[255] = { 0 };
+	strcpy_s(nkPath, argv[argc-1]);
+	char imgPath[255] = { 0 };
+	char outPath[255] = { 0 };
+	GetPrivateProfileString("Img", "imgPath", ".\\samp.img", imgPath, 255, ".\\whitebox.ini");
+	GetPrivateProfileString("Img", "outPath", NULL, outPath, 255, ".\\whitebox.ini");
+	int nkAddrOffset = GetPrivateProfileInt("Img", "nkAddrOffset", 0x81000, ".\\whitebox.ini");
+	int batAddrOffset = GetPrivateProfileInt("Img", "batAddrOffset", 0x4081000, ".\\whitebox.ini");
+	if (outPath[0] == NULL)
+	{
+		if (GetTempPath(255, outPath) == 0)
+		{
+			puts("Error - Can't get TEMP path");
+			return 1;
+		}
+		if (GetTempFileName(outPath, "wboutimg", 0, outPath) == 0)
+		{
+			puts("Error - Can't get WBOUTIMG file name");
+			return 1;
+		}
+	}
+
+
+	printf("====================Start write img====================\n");
 	printf("nkPath:...............%s\n", nkPath);
+	printf("outPath:..............%s\n", outPath);
 	printf("nkAddrOffset:.........%#x\n", nkAddrOffset);
 	printf("batAddrOffset:........%#x\n", batAddrOffset);
 	printf("batBuffer:............%s\n", batBuffer);
 
 
-	if (!CopyFile(imgPath, "./out.img", FALSE))
+	if (!CopyFile(imgPath, outPath, FALSE))
 	{
-		puts("Error - Can't copy OUT.img");
+		puts("Error - Can't copy %%OUT.img%%");
 		return 1;
 	}
+
 
 	HANDLE hFileNk = CreateFile(
 		nkPath, // 文件路径
@@ -156,9 +228,8 @@ int main(int argc,char **argv)
 		puts("Error - Can't open NK.bin");
 		return 1;
 	}
-
 	HANDLE hFileImg = CreateFile(
-		"./out.img", // 文件路径
+		outPath, // 文件路径
 		GENERIC_WRITE, // 读取权限
 		0, // 不共享
 		NULL, // 默认安全属性
@@ -171,6 +242,8 @@ int main(int argc,char **argv)
 		return 1;
 	}
 
+	printf("Start write OUT.img: ");
+	printf("********************");
 	long long int iFileNkSize = GetFileSize(hFileNk, NULL);
 	OVERLAPPED overlappedNk = { 0 };
 	overlappedNk.Offset = 0; // 从偏移位置 0 开始读取
@@ -179,7 +252,6 @@ int main(int argc,char **argv)
 	OVERLAPPED overlappedImg = { 0 };
 	overlappedImg.Offset = nkAddrOffset; // 从偏移位置 0x81000 开始写入
 	overlappedImg.OffsetHigh = 0;
-
  	char buffer[255] = { 0 }; // 缓冲区
 	DWORD bytesRead = 0;
 	DWORD bytesWritten = 0;
@@ -211,7 +283,11 @@ int main(int argc,char **argv)
 		}
 		overlappedNk.Offset += 128;
 		overlappedImg.Offset += 128;
+		if (i % 200 == 0)
+			printPrct(20, i * 100 / ((iFileNkSize / 128) + 1));
 	}
+	puts("");
+
 
 	overlappedImg.Offset = batAddrOffset;
 	if (!WriteFile(
@@ -227,9 +303,104 @@ int main(int argc,char **argv)
 	}
 	CloseHandle(hFileNk);
 	CloseHandle(hFileImg);
-	printf("========================Done===========================\n");
-	printf("Starting 86Box at %s, Config file %s\n", emulPath, profilePath);
-	if ((int)ShellExecute(NULL, "open", emulPath, profilePath, emulDir, SW_SHOWNORMAL) <= 31)
-		printf("Error - Can't start 86Box\n");
+	printf("====================Write img done=====================\n"
+		   "\n");
+
+
+
+
+
+
+	char emulPath[255] = { 0 };
+	char hdCfgKey[255] = { 0 };
+	char proOutPath[255] = { 0 };
+	char emulMch[255] = { 0 };
+	char nvrPath[255] = { 0 };
+	char nvrOutPath[255] = { 0 };
+	char nvrOutDir[255] = { 0 };
+	char emulCommand[255] = { 0 };
+	GetPrivateProfileString("86Box", "emulPath", "86Box.exe", emulPath, 255, ".\\whitebox.ini");
+	GetPrivateProfileString("86Box", "hdCfgKey", "hdd_01_fn", hdCfgKey, 255, ".\\whitebox.ini");
+	GetPrivateProfileString("86Box", "enulMch", "p54tp4xe", emulMch, 255, ".\\whitebox.ini");
+	GetPrivateProfileString("86Box", "nvrPath", NULL, nvrPath, 255, ".\\whitebox.ini");
+	GetPrivateProfileString("86Box", "proOutPath", NULL, proOutPath, 255, ".\\whitebox.ini");
+	GetPrivateProfileString("86Box", "nvrOutPath", NULL, nvrOutPath, 255, ".\\whitebox.ini");
+	GetPrivateProfileString("86Box", "nvrOutDir", NULL, nvrOutDir, 255, ".\\whitebox.ini");
+
+	if (proOutPath[0] == NULL)
+	{
+		if (GetTempPath(255, proOutPath) == 0)
+		{
+			puts("Error - Can't get TEMP path");
+			return 1;
+		}
+		if (GetTempFileName(proOutPath, "wbproout", 0, proOutPath) == 0)
+		{
+			puts("Error - Can't get WBPROOUT file name");
+			return 1;
+		}
+	}
+
+	if (nvrOutPath[0] == NULL && nvrOutDir[0] == NULL)
+	{
+		if (GetTempPath(255, nvrOutPath) == 0)
+		{
+			puts("Error - Can't get TEMP path");
+			return 1;
+		}
+		sprintf_s(nvrOutDir, "%s\\nvr\\", nvrOutPath);
+		sprintf_s(nvrOutPath, "%s\\nvr\\%s.nvr", nvrOutPath, emulMch);
+	}
+
+	if (nvrPath[0] == NULL)
+	{
+		sprintf_s(nvrPath, ".\\nvr\\%s.nvr", emulMch);
+	}
+
+	printf("====================Starting 86Box=====================\n");
+	printf("outPath:..............%s\n", outPath);
+	printf("profilePath:..........%s\n", profilePath);
+	printf("proOutPath:...........%s\n", proOutPath);
+	printf("emulPath:.............%s\n", emulPath);
+	printf("emulMchName...........%s\n", emulMch);
+	printf("hdCfgKey..............%s\n", hdCfgKey);
+	printf("nvrPath...............%s\n", nvrPath);
+	printf("nvrOutPath............%s\n", nvrOutPath);
+	printf("nvrOutDir.............%s\n", nvrOutDir);
+
+	if (!CopyFile(profilePath, proOutPath, FALSE))
+		puts("Warning - Can't copy %%PROOUT.cfg%%");
+	if (!CreateDirectory(nvrOutDir, NULL))
+		puts("Warning - Can't create %%NVRDIR%%");
+	if (!CopyFile(nvrPath, nvrOutPath, FALSE))
+		puts("Warning - Can't copy %%MCH.nvr%%");
+	WritePrivateProfileString("Hard disks", "hdd_01_fn", outPath, proOutPath);
+
+	sprintf_s(emulCommand, "-V WhiteboxEmulator %s", proOutPath);
+	printf("Starting 86Box as %s %s\n", emulPath, emulCommand);
+	SHELLEXECUTEINFO ShExecInfo = { 0 };
+	ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+	ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+	ShExecInfo.hwnd = NULL;
+	ShExecInfo.lpVerb = NULL;
+	ShExecInfo.lpFile = emulPath;
+	ShExecInfo.lpParameters = emulCommand;
+	ShExecInfo.lpDirectory = NULL;
+	ShExecInfo.nShow = SW_SHOWNORMAL;
+	ShExecInfo.hInstApp = NULL;
+	ShellExecuteEx(&ShExecInfo);
+	WaitForSingleObject(ShExecInfo.hProcess, INFINITE);
+	printf("====================86Box closed=======================\n"
+		   "\n");
+
+	Sleep(200);
+	if (!DeleteDirectory(nvrOutDir))
+		printf("Warning - Can't delete %%NVRDIR%%");
+	if (!DeleteFile(proOutPath))
+		printf("Warning - Can't delete %%PROOUT.cfg%%");
+	if (!DeleteFile(outPath))
+		printf("Warning - Can't delete %%OUT.IMG%%");
+	printf("TEMP files removed.\n");
+
 	return 0;
 }
